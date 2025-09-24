db1dae5519aacf68eed1af79be948146eaedc9b6
Sorin Badea <sorinbadea@Mac.lan>
2025-09-24 22:27:15 +0200

    reorganize images folders process image recognition to all the pictures from a directory

--- cnn.py
+++ cnn.py
@@ -15,12 +15,14 @@ class ConvolutionNN:
         ratio = self._image.width / width
         self._image.thumbnail((width, int(self._image.height/ratio)), Image.Resampling.LANCZOS)
         grayscale = ImageOps.invert(self._image.convert('L'))
-        self._image.show()
+        if self._verbose:
+            self._image.show()
         self._array = np.array(grayscale)
         self._image_rows = self._array.shape[0]
         self._image_cols = self._array.shape[1]
-        print("initial image matrix")
-        print(self._array)
+        if self._verbose:
+            print("initial image matrix")
+            print(self._array)
 
     def convolution2d(self):
         # Normalize image to [0, 1]
@@ -32,18 +34,19 @@ class ConvolutionNN:
                 conv_value = np.sum(normalized_array[i:i+self._kernel_rows, j:j+self._kernel_cols] * self._kernel)
                 feature_map[i , j] = conv_value
         self._feature_map = feature_map
-    
+
     def ReLU(self):
         self._activated_map = np.maximum(0, self._feature_map)
 
-    def max_pooling(self, size, stride=3):
+    def max_pooling(self, size=3, stride=2):
         rows, cols = self._activated_map.shape
         pooled_rows = (rows - size) // stride + 1
         pooled_cols = (cols - size) // stride + 1
         pooled_map = np.zeros((pooled_rows, pooled_cols))
         for i in range(pooled_rows):
             for j in range(pooled_cols):
-                pooled_map[i, j] = np.max(self._activated_map[i*stride:i*stride+size, j*stride:j*stride+size])
+                pooled_map[i, j] = np.max(self._activated_map
+                    [i * stride: i * stride + size, j * stride : j * stride + size])
         return pooled_map
 
     def print_array(self, text, array):
@@ -51,30 +54,20 @@ class ConvolutionNN:
             print("------" + text + "--------")
             print(array)
 
-    def process(self):
+    def process(self, pool_size, pool_stride):
         self.convolution2d()
         self.print_array("Feature map", self._feature_map)
         self.ReLU()
         self.print_array("Activated map", self._activated_map)
-        self._pooled_map = self.max_pooling(3)
+        self._pooled_map = self.max_pooling(pool_size, pool_stride)
         h_pool, w_pool = self._pooled_map.shape
         # experimental
-        # reduce pooled map to max 9 elements
-        while h_pool * w_pool >= 9:
-            self._activated_map = self._pooled_map
-            self._pooled_map = self.max_pooling(3)
-            h_pool, w_pool = self._pooled_map.shape
+        # reduce pooled map to max 12 elements
         self.print_array("Pooled map", self._pooled_map)
+        while h_pool * w_pool > 9:
+            self._activated_map = self._pooled_map
+            self._pooled_map = self.max_pooling(pool_size, pool_stride)
+            h_pool, w_pool = self._pooled_map.shape
+            self.print_array("Pooled map", self._pooled_map)
         return self._pooled_map
 
-    def report(self):
-        # Or keep RGB channels (3D array with values 0-255)
-        #rgb_array = np.array(image)
-        print(f"Image shape: {self._array.shape}")  # (height, width) or (height, width, channels)
-        print("--------------------------")
-        print(f"Kernel shape: {self._kernel.shape}")
-        print("----------------------------")
-        print(f"Featured map: {self._feature_map.shape}")
-        print(f"Activated map: {self._activated_map.shape}")
-        print(f"Pooled map: {self._pooled_map.shape}")
-        print(f"pooled map: {self._pooled_map}")
--- main.py
+++ main.py
@@ -1,33 +1,48 @@
 import cnn
 import sys
+import os
 from PIL import Image
 import filters
 
-def main(image_path):
-    conv = cnn.ConvolutionNN(image_path)
+POOL_SIZE = 3
+POOL_STRIDE = 2
+
+def get_files_from_directory(directory):
+    """
+    @param directory: Directory path
+    Generator to retrieve a list of files in the specified directory.
+    """
+    files_only = [f for f in os.listdir(directory) if os.path.isfile(os.path.join(directory, f))]
+    for f in files_only:
+        yield f
+
+def process(image_path):
+    conv = cnn.ConvolutionNN(image_path, False)
     conv.image_resize(24)
     kernels = [filters.kernel_one_1, filters.kernel_one_2, filters.kernel_one_3]
+    #kernels = [filters.kernel_two_1, filters.kernel_two_2, filters.kernel_two_3]
     flat_layer = []
     for kernel in kernels:
         conv.kernel_load(kernel)
-        pooled = conv.process()
+        pooled = conv.process(POOL_SIZE, POOL_STRIDE)
         flat_layer.append(pooled)
         print("Pooled output for kernel:", len(flat_layer))
         print(pooled)
     
-
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        image_path = sys.argv[1]
         try:
-            main(image_path)
+            image_path = sys.argv[1]
+            for image in get_files_from_directory(image_path):
+                print("Processing image:", image)
+                process(image_path + "/" + image)
         except FileNotFoundError:
-            print(f"Error: File '{image_path}' not found")
+            print(f"Error: File '{image}' not found")
          # Handle the error (set default, raise custom exception, etc.)
         except Image.UnidentifiedImageError:
-            print(f"Error: '{image_path}' is not a valid image file")
+            print(f"Error: '{image}' is not a valid image file")
         except PermissionError:
-            print(f"Error: Permission denied accessing '{image_path}'")
+            print(f"Error: Permission denied accessing '{image}'")
         except Exception as e:
             print(f"Unexpected error opening image: {e}")
     else:
