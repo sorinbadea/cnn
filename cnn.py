44d1f80525144b69d1dec7a26921d0a77c17d63e
Sorin Badea <sorinbadea@Mac.lan>
2025-09-27 09:03:21 +0200

    add some comments add more samples to parse some refactoring and code improvements add -f and -d program arguments

--- cnn.py
+++ cnn.py
@@ -1,5 +1,6 @@
 from PIL import Image, ImageOps
 import numpy as np
+import tensorflow as tf
 
 class ConvolutionNN:
     def __init__(self, image_path, verbose=False):
@@ -7,24 +8,34 @@ class ConvolutionNN:
         self._verbose = verbose
 
     def kernel_load(self, array):
+        """
+        load a speciffic filter or kernel
+        @array: kernel pattern
+        """
         self._kernel = np.array(array)
-        self._kernel_rows = self._kernel.shape[0]
-        self._kernel_cols = self._kernel.shape[1]
+        self._kernel_rows, self._kernel_cols = self._kernel.shape
 
     def image_resize(self, width=24):
+        """
+        resize input image to width and invert the gray colors
+        @param width: width to resize
+        """
         ratio = self._image.width / width
         self._image.thumbnail((width, int(self._image.height/ratio)), Image.Resampling.LANCZOS)
         grayscale = ImageOps.invert(self._image.convert('L'))
         if self._verbose:
             self._image.show()
         self._array = np.array(grayscale)
-        self._image_rows = self._array.shape[0]
-        self._image_cols = self._array.shape[1]
+        self._image_rows, self._image_cols = self._array.shape
         if self._verbose:
             print("initial image matrix")
+            print("------ height ", self._array.shape[0], " width ", self._array.shape[1])
             print(self._array)
 
     def convolution2d(self):
+        """
+        apply the convolution algorith to the image
+        """
         # Normalize image to [0, 1]
         normalized_array = np.round(self._array / 255, 2)
         feature_map = np.zeros((self._image_rows - self._kernel_rows + 1, self._image_cols - self._kernel_cols + 1))
@@ -39,6 +50,10 @@ class ConvolutionNN:
         self._activated_map = np.maximum(0, self._feature_map)
 
     def max_pooling(self, size=3, stride=2):
+        """
+        apply the max pooling step, retrive the maximum value from
+        the featured map (convolution result)
+        """
         rows, cols = self._activated_map.shape
         pooled_rows = (rows - size) // stride + 1
         pooled_cols = (cols - size) // stride + 1
@@ -51,23 +66,30 @@ class ConvolutionNN:
 
     def print_array(self, text, array):
         if self._verbose:
-            print("------" + text + "--------")
+            print("------" + text + "------- height ", array.shape[0], " width ", array.shape[1])
             print(array)
 
     def process(self, pool_size, pool_stride):
+        """
+        apply the process of image
+        normalization, convolution, ReLU, and max_pooling
+        @pool_size: the size, width and height of the pooling array
+        @pool_stride: value to shift on the right and down on each step of max pooling
+        """
         self.convolution2d()
         self.print_array("Feature map", self._feature_map)
         self.ReLU()
         self.print_array("Activated map", self._activated_map)
         self._pooled_map = self.max_pooling(pool_size, pool_stride)
         h_pool, w_pool = self._pooled_map.shape
-        # experimental
-        # reduce pooled map to max 12 elements
+        # reduce pooled map to max 4 elements
         self.print_array("Pooled map", self._pooled_map)
-        while h_pool * w_pool > 9:
+        while h_pool * w_pool > 4:
             self._activated_map = self._pooled_map
             self._pooled_map = self.max_pooling(pool_size, pool_stride)
-            h_pool, w_pool = self._pooled_map.shape
             self.print_array("Pooled map", self._pooled_map)
+            h_pool, w_pool = self._pooled_map.shape
+        #prediction = tf.reduce_mean(self._pooled_map).numpy()
+        #print("Prediction score (mean pooled activation):", prediction)
         return self._pooled_map
 
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import cnn
 import sys
 import os
+import cv2
 from PIL import Image
 import filters
 
 POOL_SIZE = 3
 POOL_STRIDE = 2
-
+REDUCED_WIDTH = 20
 def get_files_from_directory(directory):
     """
     @param directory: Directory path
@@ -16,14 +17,26 @@ def get_files_from_directory(directory):
     for f in files_only:
         yield f
 
-def process(image_path):
-    conv = cnn.ConvolutionNN(image_path, False)
-    conv.image_resize(24)
-    kernels = [filters.kernel_one_1, filters.kernel_one_2, filters.kernel_one_3]
-    #kernels = [filters.kernel_two_1, filters.kernel_two_2, filters.kernel_two_3]
+def edge_image(image_file):
+    try:
+        img = cv2.imread(image_file, cv2.IMREAD_GRAYSCALE)
+        edge_image = cv2.Canny(img, 30, 170)
+        cv2.imwrite('tmp/edge.jpeg', edge_image)
+    except Exception as e:
+        print(f"Unexpected error opening image: {e}")
+        exit(1)
+
+def process(image_path, debug=False):
+    """
+    process the image loaded from image_path
+    @param image_path : image location
+    @param debug: enable or disable
+    """
+    conv = cnn.ConvolutionNN(image_path, debug)
+    conv.image_resize(REDUCED_WIDTH)
     flat_layer = []
-    for kernel in kernels:
-        conv.kernel_load(kernel)
+    for key in filters.kernels_digit_one:
+        conv.kernel_load(filters.kernels_digit_one[key])
         pooled = conv.process(POOL_SIZE, POOL_STRIDE)
         flat_layer.append(pooled)
         print("Pooled output for kernel:", len(flat_layer))
@@ -32,17 +45,24 @@ def process(image_path):
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         try:
-            image_path = sys.argv[1]
-            for image in get_files_from_directory(image_path):
-                print("Processing image:", image)
-                process(image_path + "/" + image)
+            if sys.argv[1] == "-f":
+                image_path = sys.argv[2]
+                process(image_path, True)
+            elif sys.argv[1] == "-d":
+                image_path = sys.argv[2]
+                for image in get_files_from_directory(image_path):
+                    print("Processing image:", image)
+                    process(image_path + "/" + image, False)
+            else:
+                print("Usage python main.py -f [image file]")
+                print("      python main.py -d [folder_with images]")
         except FileNotFoundError:
-            print(f"Error: File '{image}' not found")
+            print(f"Error: File '{image_path}' not found")
          # Handle the error (set default, raise custom exception, etc.)
         except Image.UnidentifiedImageError:
-            print(f"Error: '{image}' is not a valid image file")
+            print(f"Error: '{image_path}' is not a valid image file")
         except PermissionError:
-            print(f"Error: Permission denied accessing '{image}'")
+            print(f"Error: Permission denied accessing '{image_path}'")
         except Exception as e:
             print(f"Unexpected error opening image: {e}")
     else:
