update the threshold computation recover the euclid distance, not used yet add some new houses as training images some refactoring in cnn.py, update filters fix a bug in invert criteria add results.txt

analyzer.py
@@ -16,7 +16,7 @@ def is_match_distance(new_values, trained_values):
     distance = euclidean_distance(new_values, trained_values)
     # try to evaluate an appropiate threshold
     nv_average = np.sum(np.array(new_values))/len(new_values)
-    threshold = nv_average * 0.1
+    threshold = nv_average * 0.05
     return distance < threshold, distance
 
 def iterate_in_samples(trained_filter, input_pooled):
@@ -30,24 +30,30 @@ def iterate_in_samples(trained_filter, input_pooled):
 def evaluate_filter(trained_filter, input_pooled, verbose=False):
     """
     Evaluates a single pooled filter result against trained 
-    patterns using euclidean distance;
+    patterns using euclidean distance; returns a triple of
+    matches, not matches and euclidian distance
     @param trained_filter: list of trained samples for a kernel
     @param input_pooled: new input samples to evaluate
     """
     matches = 0
     not_matches = 0
+    min_distance = 255.0
     for pool_row, train_row in iterate_in_samples(trained_filter, input_pooled):
         match, distance = is_match_distance(pool_row, train_row)
         if match:
             matches += 1
         else:
             not_matches += 1
-    return matches, not_matches
+        # evaluate the minial distance for this kernel
+        if min_distance > round(distance.item(), 2):
+            min_distance = round(distance.item(), 2)
+    return matches, not_matches, min_distance
 
-def evaluate(pooled_maps, verbose=False):
+def evaluate(pooled_maps, shape_index, verbose=False):
     """
     Returns a map with the result of evaluation for each kernel;
-    @param pooled_maps: map of kernel shapes to pooled outputs@
+    @param pooled_maps: map of kernel shapes to pooled outputs
+    @param shape_index: shape index
     """
     result = {}
     db = DataBaseInterface('localhost','myapp','postgres','password',5432)
@@ -58,9 +64,14 @@ def evaluate(pooled_maps, verbose=False):
             return None
         else:
             result[key] = evaluate_filter(trained_filter, pooled_maps[key], verbose)
+    if verbose:
+        print("analyse result for shape ", shape_index + 1)
+        print(result)
     db.database_disconnect()
     matches = [ result[key][0] for key in result ]
+    min_distances = [ result[key][2] for key in result ]
     total_matches = sum(1 for m in matches if m > 0)
+    ### print("minmal distances ",min_distances, " total ", sum(min_distances))
     return total_matches/len(result)
 
 if __name__ == "__main__":

cnn.py
@@ -4,7 +4,7 @@ from tensorflow.keras.layers import MaxPooling2D
 
 class ConvolutionNN:
     def __init__(self, image_path, verbose=False):
-        self._image = Image.open(image_path)
+        self._image_path = image_path
         self._verbose = verbose
 
     def kernel_load(self, array):
@@ -15,23 +15,34 @@ class ConvolutionNN:
         self._kernel = np.array(array)
         self._kernel_rows, self._kernel_cols = self._kernel.shape
 
-    def image_resize(self, width=24):
+    def pre_processing(self, width = 48):
         """
-        resize input image to required width
-        @param width: width to resize
+        resize image, convert to gray-scale and invert the gray-scale
+        if the image is too dark, (helps convolution)
+        @param width: image width to resize
         """
+        self._image = Image.open(self._image_path)
         ratio = self._image.width / width
-        self._image.thumbnail((width, int(self._image.height/ratio)), Image.Resampling.LANCZOS)
-
-    def grayscale(self):
-        grayscale = ImageOps.invert(self._image.convert('L'))
-        if self._verbose:
-            self._image.show()
-        self._array = np.array(grayscale)
+        self._image.thumbnail((width, int(self._image.height/ratio)), Image.Resampling.LANCZOS)    
+        """
+        convert to grayscale
+        """
+        image = self._image.convert('L')
+        img_array = np.array(image)
+        # Count pixels that are very dark (e.g., < 50 out of 255)
+        black_pixels = np.sum(img_array < 50)
+        total_pixels = img_array.size
+        """
+        invert the gray scale if too bright
+        """
+        if black_pixels / total_pixels < 0.34:
+            image = ImageOps.invert(image)
+        self._array = np.array(image)
         self._image_rows, self._image_cols = self._array.shape
         if self._verbose:
             print("initial image matrix")
             print("------ height ", self._array.shape[0], " width ", self._array.shape[1])
+            image.show()
             print(self._array)
 
     def run_convolution(self, normalized_array):